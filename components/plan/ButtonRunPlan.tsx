026d29d100eb4482430c38fe921e36052d2ea19e
Nils Weigel <nils.weigel@gmail.com>
2023-12-18 21:51:13 +0100

    display toast when scheduling a plan

--- components/plan/ButtonRunPlan.tsx
+++ components/plan/ButtonRunPlan.tsx
@@ -1,10 +1,20 @@
 import {PlanListItem} from "../../lib/PlanListItem";
-import {Button} from "@chakra-ui/react";
+import {Button, useToast} from "@chakra-ui/react";
 import {apiHost} from "../../lib/api";
 
 export function ButtonRunPlan({plan}: {plan: PlanListItem}) {
+    const toast = useToast()
+
     return (
-        <Button onClick={() => fetch(`${apiHost}/plans/${plan.uuid}/run`, {method: "POST"})}>
+        <Button onClick={() => {
+            const runPlanRequest = fetch(`${apiHost}/plans/${plan.uuid}/run`, {method: "POST"})
+
+            toast.promise(runPlanRequest, {
+                success: { title: 'Successfully scheduled', description: 'The Plan has been scheduled for Execution' },
+                error: { title: 'Failed', description: 'Failed to schedule Plan execution' },
+                loading: { title: 'Loading', description: 'Please wait' },
+            })
+        }}>
             🏇🏽 Run
         </Button>
     );
