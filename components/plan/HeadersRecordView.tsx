ee50052b4acbb912203a909119b9e2ee76696c28
Nils Weigel <nils.weigel@gmail.com>
2023-12-26 20:57:00 +0100

    added missing key

--- components/plan/HeadersRecordView.tsx
+++ components/plan/HeadersRecordView.tsx
@@ -4,8 +4,8 @@ import {ListItem, UnorderedList} from "@chakra-ui/react";
 export function HeadersRecordView({headers}: {headers: HeadersRecord}) {
     return (
         <UnorderedList>
-            {Object.keys(headers).map(key => headers[key].map(value =>
-                <ListItem>
+            {Object.keys(headers).map((key, index) => headers[key].map(value =>
+                <ListItem key={`${key}-${index}`}>
                     {key}: {value}
                 </ListItem>
             ))}
